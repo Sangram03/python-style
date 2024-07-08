d09c0735f62c861221710f6166cfdd3efecd132f
Sangram Das <121278559+Sangram03@users.noreply.github.com>
2024-07-08 20:38:41 +0530

    Update style.py
    hii

--- style.py
+++ style.py
@@ -1,127 +1,265 @@
-# Import turtle package 
+import turtle
 
-import turtle 
+draw = turtle.Turtle()
 
-  
-# Creating a turtle object(pen) 
+def curve():
+    draw.pen(pencolor="white", pensize=3, speed=5)
+    for i in range(200):
+        draw.rt(1)
+        draw.fd(1)
 
-pen = turtle.Turtle() 
+def heart():
+    draw.pen(pencolor="white",fillcolor="red", pensize=3, speed=5)
+    draw.shape("turtle")
+    draw.shapesize(1,1,1)
+    draw.begin_fill()
+    draw.lt(50)
+    draw.fd(113)
+    curve()
+    draw.lt(120)
+    curve()
+    draw.fd(112)
+    draw.end_fill()
 
-  
-# Defining a method to draw curve 
+    draw.hideturtle()
 
-def curve(): 
 
-    for i in range(250): 
+window = turtle.Screen()
+window.bgcolor('black')
 
-  
+draw.penup()
+draw.goto(-80,300)
+draw.pendown()
+draw.shapesize(1,2,1)
 
-        # Defining step by step curve motion 
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
 
-        pen.right(1) 
+draw.begin_fill()
 
-        pen.forward(1) 
+draw.fd(160)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
 
-  
-# Defining method to draw a full heart 
+draw.fd(140)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(160)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(140)
+draw.left(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(25)
 
-def heart(): 
+draw.end_fill()
 
-  
+draw.penup()
+draw.goto(-550,-20)
+draw.pendown()
 
-    # Set the fill color to red 
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=2)
+draw.begin_fill()
 
-    pen.fillcolor('red') 
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(165)
+draw.lt(90)
+draw.fd(115)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(140)
+draw.rt(90)
+draw.fd(190)
+draw.rt(90)
 
-  
+draw.end_fill()
 
-    # Start filling the color 
+draw.penup()
+draw.fd(140)
 
-    pen.begin_fill() 
+draw.fd(70)
 
-  
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
 
-    # Draw the left line 
+draw.rt(90)
+draw.fd(190)
+draw.lt(90)
+draw.pendown()
+draw.circle(60)
+draw.lt(90)
+draw.penup()
+draw.fd(20)
+draw.rt(90)
+draw.pendown()
+draw.circle(40)
+draw.rt(90)
+draw.penup()
+draw.fd(20)
+draw.lt(90)
 
-    pen.left(140) 
+draw.end_fill()
 
-    pen.forward(113) 
+draw.fd(100)
+draw.pendown()
 
-  
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
 
-    # Draw the left curve 
+draw.lt(100)
+draw.fd(120)
+draw.rt(100)
+draw.fd(20)
+draw.rt(80)
+draw.fd(100)
+draw.lt(80)
+draw.fd(20)
+draw.lt(80)
+draw.fd(100)
+draw.rt(80)
+draw.fd(20)
+draw.rt(100)
+draw.fd(120)
+draw.rt(80)
+draw.fd(50)
+draw.lt(180)
 
-    curve() 
+draw.end_fill()
 
-    pen.left(120) 
+draw.penup()
+draw.fd(100)
+draw.pendown()
 
-  
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
 
-    # Draw the right curve 
+draw.lt(90)
+draw.fd(120)
+draw.rt(90)
+draw.fd(80)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(30)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(30)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(80)
 
-    curve() 
+draw.end_fill()
 
-  
+draw.penup()
+draw.rt(180)
+draw.fd(200)
+draw.pendown()
 
-    # Draw the right line 
 
-    pen.forward(112) 
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=2)
+draw.begin_fill()
 
-  
+draw.lt(90)
+draw.fd(50)
+draw.lt(30)
+draw.fd(80)
+draw.rt(120)
+draw.fd(20)
+draw.rt(60)
+draw.fd(60)
+draw.lt(180)
+draw.rt(60)
+draw.fd(60)
+draw.rt(60)
+draw.fd(20)
+draw.rt(120)
+draw.fd(80)
+draw.lt(30)
+draw.fd(50)
+draw.rt(90)
+draw.fd(20)
+draw.rt(180)
 
-    # Ending the filling of the color 
+draw.end_fill()
 
-    pen.end_fill() 
+draw.penup()
+draw.fd(120)
+draw.pendown()
 
-  
-# Defining method to write text 
 
-def txt(): 
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
 
-  
+draw.circle(60)
+draw.lt(90)
+draw.penup()
+draw.fd(20)
+draw.pendown()
+draw.rt(90)
+draw.circle(40)
+draw.rt(90)
+draw.penup()
+draw.fd(20)
+draw.lt(90)
 
-    # Move turtle to air 
+draw.end_fill()
 
-    pen.up() 
+draw.fd(100)
+draw.circle(60, extent=60)
+draw.pendown()
 
-  
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
 
-    # Move turtle to a given position 
+draw.lt(30)
 
-    pen.setpos(-68, 95) 
+draw.fd(85)
+draw.lt(90)
+draw.fd(20)
+draw.lt(90)
+draw.fd(70)
+draw.circle(-20, extent=180)
+draw.fd(70)
+draw.lt(90)
 
-  
+draw.fd(20)
+draw.lt(90)
+draw.fd(85)
+draw.circle(40, extent=180)
 
-    # Move the turtle to the ground 
+draw.end_fill()
 
-    pen.down() 
+draw.penup()
 
-  
+draw.rt(180)
+draw.fd(35)
+draw.lt(90)
+draw.fd(140)
+draw.lt(90)
+draw.pendown()
 
-    # Set the text color to lightgreen 
+heart()
 
-    pen.color('lightgreen') 
-
-  
-
-    # Write the specified text in  
-
-    # specified font style and size 
-
-    pen.write("Point", font=( 
-
-      "Verdana", 12, "bold")) 
-
-  
-
-  
-# Draw a heart 
-heart() 
-
-  
-# Write text 
-txt() 
-
-  
-# To hide turtle 
-pen.ht()
+turtle.done()
